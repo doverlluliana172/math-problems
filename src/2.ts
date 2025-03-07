9ca3af25e2b5139d7a23b7beafbb848e32a5461e
doverlluliana172 <doverlluliana172@gmail.com>
2025-03-07 07:53:44 +0000

    Add 2.ts

---
+++ src/2.ts
@@ -0,0 +1,8 @@
+
+import { getRandomInt } from 'math-problems';
+
+const num1 = getRandomInt(0, 10);
+const num2 = getRandomInt(0, 10);
+const sum = num1 + num2;
+
+console.log(`${num1} + ${num2} = `, sum);
