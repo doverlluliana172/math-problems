a20f22043f084e1abdbdc972313c5617979ed3d5
doverlluliana172 <doverlluliana172@gmail.com>
2025-03-09 00:27:59 +0000

    Add 4.ts

---
+++ src/4.ts
@@ -0,0 +1,15 @@
+  const solveMathProblem = () => {
+    const problem1 = {
+      question: 'What is the value of x in the equation 2x + 5 = 11?',
+      answer: 3
+    };
+    const problem2 = {
+      question: 'If Sally can paint a house in 6 hours and John can paint a house in 4 hours and it takes them together 8 hours to paint two houses, how many hours does it take for each of them to paint one house?',
+      answer: 3
+    };
+    const problem3 = {
+      question: 'What is the value of x in the equation f(x) = 3x + 2?',
+      answer: 2
+    };
+    return [problem1, problem2, problem3];
+  }
