29da3873b15b584c665856ce245295a052653e1f
doverlluliana172 <doverlluliana172@gmail.com>
2025-04-11 11:32:22 +0000

    Add 31.ts

---
+++ src/31.ts
@@ -0,0 +1,12 @@
+import sympy as sp
+
+# Example variable
+x = sp.symbols('x')
+
+# Define the expression
+expr = x**2 - 3*x + 2
+
+# Simplify the expression
+simplified_expr = sp.simplify(expr)
+
+print(simplified_expr)
